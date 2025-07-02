97e42943e948aeb3ce17c9f71927d11d361323ef
lkaraba <karaba24@gmail.com>
2025-07-02 14:31:37 -0400

    formatting updates

--- circle_maze/circle_maze.ino
+++ circle_maze/circle_maze.ino
@@ -40,9 +40,9 @@ int read_left = 0;
 int IR_thresh = 500; //this should work for all. >500 is empty, <500 detects mouse
 
 // Set up behavior
-lap_ct = 0;
-prev_loc = 0; //0 = water port, 1 = right IR, 2 = middle IR, 3 = left IR
-water_delay = 5000;
+int lap_ct = 0;
+int prev_loc = 0; //0 = water port, 1 = right IR, 2 = middle IR, 3 = left IR
+int water_delay = 5000;
 
 void setup() {
   // put your setup code here, to run once:
@@ -52,44 +52,43 @@ void setup() {
 
   //mouse is starting in home port. Depending on trial setup, need to start in a different configuration
   // full circle, CCW
-  if (full_circle) && (CCW)
+  if ((full_circle) && (CCW))
   {
     servo1_right.write(open_right);
     servo1_middle.write(open_middle);
     servo1_left.write(closed_left);
   }
   // full circle, CW
-  else if (full_circle) && (!CCW)
+  else if ((full_circle) && (!CCW))
   {
     servo1_right.write(closed_right);
     servo1_middle.write(open_middle);
     servo1_left.write(open_left);
   }
   // half circle, CCW
-  else if (!full_circle) && (CCW)
+  else if ((!full_circle) && (CCW))
   {
     servo1_right.write(closed_right); //permanently closed for this trial type
     servo1_middle.write(block_right);
     servo1_left.write(closed_left);
   }
   // half circle, CW
-  else if (!full_circle) && (!CCW)
+  else if ((!full_circle) && (!CCW))
   {
     servo1_right.write(closed_right);
     servo1_middle.write(block_left);
     servo1_left.write(closed_left);
   }
-}
 
-pinMode(pump_1, OUTPUT);
-digitalWrite(pump_1, LOW);
+  pinMode(pump_1, OUTPUT);
+  digitalWrite(pump_1, LOW);
 
-pinMode(IR_water, INPUT);
-pinMode(IR_right, INPUT);
-pinMode(IR_middle, INPUT);
-pinMode(IR_left, INPUT);
+  pinMode(IR_water, INPUT);
+  pinMode(IR_right, INPUT);
+  pinMode(IR_middle, INPUT);
+  pinMode(IR_left, INPUT);
 
-//Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -100,34 +99,38 @@ void loop() {
   read_left = analogRead(IR_left);
 
 
-  if (full_circle) && (CCW)
+  if ((full_circle) && (CCW))
   {
-    if (read_right < IR_thresh) && (prev_loc == 0) //passes right IR coming from water port
+    if ((read_right < IR_thresh) && (prev_loc == 0)) //passes right IR coming from water port
     {
+      Serial.print("Right arm");
       servo1_right.write(closed_right);
       servo1_middle.write(open_middle);
       servo1_left.write(open_left);
       prev_loc = 1;
     }
-    if (read_left < IR_thresh) && (prev_loc == 1) //passes left IR coming from right arm
+    if ((read_left < IR_thresh) && (prev_loc == 1)) //passes left IR coming from right arm
     {
+      Serial.print("Left arm");
       servo1_middle.write(block_left); //block from going back
       prev_loc = 3;
     }
-    if (read_water < IR_thresh) && (prev_loc == 3)
+    if ((read_water < IR_thresh) && (prev_loc == 3))
     {
-      lap_ct = lap_ct+1;
-      if lap_ct == num_laps
+      Serial.print("Water port");
+      lap_ct = lap_ct + 1;
+      if (lap_ct == num_laps)
       {
-        servo1_left.write(close_left);
+        Serial.print("Dispensing");
+        servo1_left.write(closed_left);
         pump_ct = millis();
         while ((millis() - pump_ct) < PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, HIGH)
+          digitalWrite(pump_1, HIGH);
         }
         if ((millis() - pump_ct) >= PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, LOW)
+          digitalWrite(pump_1, LOW);
         }
         delay(water_delay);
         servo1_right.write(open_right);
@@ -140,34 +143,38 @@ void loop() {
       }
     }
   }
-  elseif (full_circle) && (!CCW)
+  else if ((full_circle) && (!CCW))
   {
-    if (read_left < IR_thresh) && (prev_loc == 0) //passes left IR coming from water port
+    if ((read_left < IR_thresh) && (prev_loc == 0)) //passes left IR coming from water port
     {
+      Serial.print("Left arm");
       servo1_left.write(closed_left);
       servo1_middle.write(open_middle);
       servo1_right.write(open_right);
       prev_loc = 3;
     }
-    if (read_right < IR_thresh) && (prev_loc == 3) //passes right IR coming from left arm
+    if ((read_right < IR_thresh) && (prev_loc == 3)) //passes right IR coming from left arm
     {
+      Serial.print("Right arm");
       servo1_middle.write(block_right); //block from going back
       prev_loc = 1;
     }
-    if (read_water < IR_thresh) & (prev_loc == 1)
+    if ((read_water < IR_thresh) & (prev_loc == 1))
     {
-      lap_ct = lap_ct+1;
-      if lap_ct == num_laps
+      Serial.print("Water port");
+      lap_ct = lap_ct + 1;
+      if (lap_ct == num_laps)
       {
-        servo1_right.write(close_right);
+        Serial.print("Dispensing");
+        servo1_right.write(closed_right);
         pump_ct = millis();
         while ((millis() - pump_ct) < PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, HIGH)
+          digitalWrite(pump_1, HIGH);
         }
         if ((millis() - pump_ct) >= PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, LOW)
+          digitalWrite(pump_1, LOW);
         }
         delay(water_delay);
         servo1_left.write(open_left);
@@ -180,34 +187,34 @@ void loop() {
       }
     }
   }
-  elseif (!full_circle) && (CCW) //always start down middle trajectory, right always blocked
+  else if ((!full_circle) && (CCW)) //always start down middle trajectory, right always blocked
   {
-    if (read_middle < IR_thresh) && (prev_loc == 0) //passes middle IR coming from water port
+    if ((read_middle < IR_thresh) && (prev_loc == 0)) //passes middle IR coming from water port
     {
       servo1_right.write(closed_right);
       servo1_middle.write(block_right);
       servo1_left.write(open_left);
       prev_loc = 2;
     }
-    if (read_left < IR_thresh) && (prev_loc == 2) //passes left IR coming from middle
+    if ((read_left < IR_thresh) && (prev_loc == 2)) //passes left IR coming from middle
     {
       servo1_middle.write(block_left); //block from going back
       prev_loc = 3;
     }
-    if (read_water < IR_thresh) & (prev_loc == 3)
+    if ((read_water < IR_thresh) & (prev_loc == 3))
     {
-      lap_ct = lap_ct+1;
-      if lap_ct == num_laps
+      lap_ct = lap_ct + 1;
+      if (lap_ct == num_laps)
       {
-        servo1_left.write(close_left);
+        servo1_left.write(closed_left);
         pump_ct = millis();
         while ((millis() - pump_ct) < PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, HIGH)
+          digitalWrite(pump_1, HIGH);
         }
         if ((millis() - pump_ct) >= PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, LOW)
+          digitalWrite(pump_1, LOW);
         }
         //no water delay here
         prev_loc == 0;
@@ -215,34 +222,34 @@ void loop() {
       } // no else here because there isn't a door to block them from continuing
     }
   }
-  elseif (!full_circle) && (!CCW)
+  else if ((!full_circle) && (!CCW))
   {
-    if (read_middle < IR_thresh) && (prev_loc == 0) //passes middle IR coming from water port
+    if ((read_middle < IR_thresh) && (prev_loc == 0)) //passes middle IR coming from water port
     {
       servo1_left.write(closed_left);
       servo1_middle.write(block_left);
       servo1_right.write(open_right);
       prev_loc = 2;
     }
-    if (read_right < IR_thresh) && (prev_loc == 2) //passes right IR coming from left arm
+    if ((read_right < IR_thresh) && (prev_loc == 2)) //passes right IR coming from left arm
     {
       servo1_middle.write(block_right); //block from going back
       prev_loc = 1;
     }
-    if (read_water < IR_thresh) & (prev_loc == 1)
+    if ((read_water < IR_thresh) & (prev_loc == 1))
     {
-      lap_ct = lap_ct+1;
-      if lap_ct == num_laps
+      lap_ct = lap_ct + 1;
+      if (lap_ct == num_laps)
       {
-        servo1_right.write(close_right);
+        servo1_right.write(closed_right);
         pump_ct = millis();
         while ((millis() - pump_ct) < PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, HIGH)
+          digitalWrite(pump_1, HIGH);
         }
         if ((millis() - pump_ct) >= PUMP_OPEN_TIME)
         {
-          digitalWrite(pump_1, LOW)
+          digitalWrite(pump_1, LOW);
         }
         //no water delay here
         prev_loc == 0;
